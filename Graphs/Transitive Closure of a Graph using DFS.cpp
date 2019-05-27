e14f430dd05469fa6d95fd72dea9886c04824852
Aman Kumar Jain <jainamandelhi@gmail.com>
2019-05-28 01:58:03 +0530

    Update Transitive Closure of a Graph using DFS.cpp

--- Graphs/Transitive Closure of a Graph using DFS.cpp
+++ Graphs/Transitive Closure of a Graph using DFS.cpp
@@ -1,38 +1,55 @@
 #include<bits/stdc++.h>
-#define ll long long
-#define pb push_back
-#define mp make_pair
 using namespace std;
-void fun(int i,int j,vector<vector<int> >v,vector<vector<int> >&arr)
+#define mp make_pair
+#define pb push_back
+#define fast ios_base::sync_with_stdio(false)
+#define ll long long
+ll mod=1000000007;
+vector<vector<ll> >v;
+vector<ll>vis;
+ll gcd(ll a,ll b)
 {
-    arr[i][j]=1;
-    for(int k=0;k<v[j].size();k++)
-        if(arr[i][v[j][k]]==0)
-            fun(i,v[j][k],v,arr);
-    return;
+    return (b==0)?a:gcd(b,a%b);
+}
+void dfs(ll st)
+{
+    vis[st]=1;
+    for(ll i=0;i<v[st].size();i++)
+    {
+        if(v[st][i]==1 && vis[i]==0)
+        {
+            vis[i]=1;
+            dfs(i);
+        }
+    }
 }
 int main()
 {
-    int n;
-    cin>>n;
-    vector<vector<int> >v(n);
-    for(int i=0;i<5;i++)
+    ll t;
+    cin>>t;
+    while(t--)
     {
-        int a,b;
-        cin>>a>>b;
-        v[a].pb(b);
-    }
-    vector<vector<int> >arr(n,vector<int>(n,0));
-    for(int i=0;i<n;i++)
-        arr[i][i]=1;
-    for(int i=0;i<n;i++)
-    {
-        fun(i,i,v,arr);
-    }
-    for(int i=0;i<n;i++)
-    {
-        for(int j=0;j<n;j++)
-            cout<<arr[i][j]<<" ";
+        ll n;
+        cin>>n;
+        v=vector<vector<ll> >(n,vector<ll>(n));
+        vector<vector<ll> >ans(n,vector<ll>(n));
+        for(ll i=0;i<n;i++)
+            for(ll j=0;j<n;j++)
+                cin>>v[i][j];
+        for(ll i=0;i<n;i++)
+        {
+            vis=vector<ll>(n);
+            dfs(i);
+            ans[i][i]=1;
+            for(ll j=0;j<v[i].size();j++)
+            {
+                if(vis[j]==1)
+                    ans[i][j]=1;
+            }
+        }
+        for(ll i=0;i<n;i++)
+            for(ll j=0;j<n;j++)
+                cout<<ans[i][j]<<" ";
         cout<<endl;
     }
     return 0;

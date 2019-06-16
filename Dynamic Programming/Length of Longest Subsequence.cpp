65dd1cdcb4b1fe6ff5f4185b50fbed02a442af17
Aman Kumar Jain <39856232+jainamandelhi@users.noreply.github.com>
2019-06-16 19:52:37 +0530

    Update Length of Longest Subsequence.cpp

--- Dynamic Programming/Length of Longest Subsequence.cpp
+++ Dynamic Programming/Length of Longest Subsequence.cpp
@@ -2,7 +2,6 @@ int Solution::longestSubsequenceLength(const vector<int> &A) {
     int n=A.size();
     if(n==0)
         return 0;
-    char ch[n];
     int inc[n];
     int dec[n];
     for(int i=0;i<n;i++)
@@ -14,17 +13,24 @@ int Solution::longestSubsequenceLength(const vector<int> &A) {
     {
         for(int j=i-1;j>=0;j--)
         {
-            if(A[i]>A[j] && inc[j]+1>inc[i])
+            if(A[i]>A[j])
             {
-                inc[i]=inc[j]+1;
-                //break;
-            }
-            if(A[i]<A[j])
-            {
-                dec[i]=max(dec[i],max(dec[j]+1,inc[j]+1));
-                //break;
+                inc[i]=max(inc[j]+1,inc[i]);
             }
         }
     }
-    return max(inc[n-1],dec[n-1]);
+    for(int i=n-2;i>=0;i--)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if(A[i]>A[j])
+            {
+                dec[i]=max(dec[j]+1,dec[i]);
+            }
+        }
+    }
+    int maxi=INT_MIN;
+    for(int i=0;i<n;i++)
+        maxi=max(maxi,inc[i]+dec[i]-1);
+    return maxi;
 }

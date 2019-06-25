cc3bd59940ef2fd58b6a8c2786f4f02b88b39350
Aman Kumar Jain <jainamandelhi@gmail.com>
2019-06-26 00:12:50 +0530

    Create Heap Sort.cpp

---
+++ Heap/Heap Sort.cpp
@@ -0,0 +1,35 @@
+/*This is a function problem.You only need to complete the function given below*/
+/* Main function to do heap sort. This function uses buildHeap()
+   and heapify()
+void heapSort(int arr[], int n)  {
+    buildHeap(arr, n);
+    for (int i=n-1; i>=0; i--)  {
+        swap(arr[0], arr[i]);
+        heapify(arr, i, 0);
+    }
+} */
+// The functions should be written in a way that array become sorted 
+// in increasing order when above heapSort() is called.
+// To heapify a subtree rooted with node i which is  an index in arr[]. 
+// n is size of heap. This function  is used in above heapSor()
+void heapify(int arr[], int n, int i)  {
+    int l=2*i+1;
+    int r=2*i+2;
+    int highest=i;
+    if(l<n && arr[l]>arr[highest])
+        highest=l;
+    if(r<n && arr[r]>arr[highest])
+        highest=r;
+    swap(arr[highest],arr[i]);
+    if(highest!=i)
+        heapify(arr,n,highest);
+      // Your Code Here
+}
+// Rearranges input array so that it becomes a max heap
+void buildHeap(int arr[], int n)  {
+    for(int i=n/2-1;i>=0;i--)
+    {
+        heapify(arr,n,i);
+    }
+    // Your Code Here
+}

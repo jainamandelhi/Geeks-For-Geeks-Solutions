cc405d488dd054779b59a5f1b02d7e7e58ab5515
Aman Kumar Jain <jainamandelhi@gmail.com>
2019-06-27 07:18:16 +0530

    Create Implement two stacks in an array.cpp

---
+++ Stack/Implement two stacks in an array.cpp
@@ -0,0 +1,29 @@
+void twoStacks :: push1(int x)
+{
+    top1++;
+    arr[top1]=x;
+}
+   
+/* The method push to push element into the stack 2*/
+void twoStacks ::push2(int x)
+{
+    top2--;
+    arr[top2]=x;
+}
+   
+/* The method pop to pop element from the stack 1 */
+//Return the popped element
+int twoStacks ::pop1()
+{
+    if(top1<0)
+        return -1;
+    return arr[top1--];
+}
+/* The method pop to pop element from the stack 2 */
+//Return the popped element
+int twoStacks :: pop2()
+{
+    if(top2>=100)
+        return -1;
+    return arr[top2++];
+}

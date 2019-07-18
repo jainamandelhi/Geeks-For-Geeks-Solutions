d4ac8de72d8fd0a56224a3a72c5d3cb31d511dbf
Aman Kumar Jain <jainamandelhi@gmail.com>
2019-07-19 01:07:11 +0530

    Create Leaders in an array.cpp

---
+++ Arrays/Leaders in an array.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define mp make_pair
+#define pb push_back
+#define fast ios_base::sync_with_stdio(false)
+#define ll long long
+ll mod=1000000007;
+ll gcd(ll a,ll b)
+{
+    return (b==0)?a:gcd(b,a%b);
+}
+int main()
+{
+    ll t,flag;
+    cin>>t;
+    while(t--)
+    {
+        ll n;
+        cin>>n;
+        ll arr[n];
+        for(ll i=0;i<n;i++)
+            cin>>arr[i];
+        ll ans[n];
+        ans[n-1]=arr[n-1];
+        //ll maxi=0;
+        for(ll i=n-2;i>=0;i--)
+        {
+            ans[i]=max(arr[i],ans[i+1]);
+        }
+        for(ll i=0;i<n;i++)
+        {
+            if(arr[i]==ans[i])
+                cout<<arr[i]<<" ";
+        }
+        cout<<endl;
+    }
+}
